2633d36b37fa4a46e2bdae7e06db0ed39b86aa99
Aayushi Ghosh <aayushighosh2092001@gmail.com>
2024-03-30 21:17:39 +0530

    second commit

--- automorphic.java
+++ automorphic.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-public class armstrong {
+public class automorphic {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int x,p,n,s=0;
+        int x,p,n,c=0,s=1;
         System.out.println("enter a number:");
         x=in.nextInt();
         n=x;
         while(x>0)
         {
             p=x%10;
-            s=s+(int)Math.pow(p,3);
+            c++;
             x=x/10;
         }
         x=n;
-        if(x==s)
-        System.out.println("armstrong");
+        s=x*x;
+        int r=s%(int)Math.pow(10,c);
+        if(r==x)
+        System.out.println("automorphic");
         else
-        System.out.println("not armstrong");
+        System.out.println("not automorphic");
     }
 }
